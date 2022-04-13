94601fad208f9d82eedca567bb7f68854999b003
Devin Ho <djxho36@gmail.com>
2022-04-13 08:39:23 -0400

    commit tree fd6497104edd5a4173f6c3d66ec0f6f91046b43a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function superbowlWin(record) { 
+    /* for (let item of element) {
+        let noWin = "undefined";
+        if (item.result === "W"){
+            return item.year;
+        } else { 
+            return noWin;
+        }
+    }
+    element.find(superbowlWin);
+} 
+*/
+
+let item = record.find(obj => obj.result === "W")
+if (item) {
+    return item.year
+}
+} 
+
